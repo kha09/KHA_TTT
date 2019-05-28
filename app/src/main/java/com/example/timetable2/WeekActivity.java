df3754d426c22ddff771e33ac4763f4a1dd4778e
KHA <gamerx6900@gmail.com>
2019-05-28 15:21:41 +0300

    WeekActivity 01

--- app/src/main/java/com/example/timetable2/WeekActivity.java
+++ app/src/main/java/com/example/timetable2/WeekActivity.java
@@ -1,9 +1,18 @@
 package com.example.timetable2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.timetable2.utils.LetterImageView;
 
 public class WeekActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -19,6 +28,7 @@ public class WeekActivity extends AppCompatActivity {
     private void setupUIViews(){
         toolbar = findViewById(R.id.ToolbarWeek);
         listView = findViewById(R.id.listWeek);
+
     }
     private void initToolbar(){
         setSupportActionBar(toolbar);
@@ -26,5 +36,39 @@ public class WeekActivity extends AppCompatActivity {
     }
     private void setupListView(){
         String[] week = getResources().getStringArray(R.array.Week);
+        WeekAdapter adapter = new WeekAdapter(this, R.layout.activity_week_single_item,null);
+        listView.setAdapter(adapter);
+        
+    }
+    public class WeekAdapter extends ArrayAdapter{
+        private int resource;
+        private LayoutInflater layoutInflater;
+        private String[] week = new String[]{};
+
+        public WeekAdapter(Context context, int resource, String[] objects) {
+            super(context, resource, objects);
+            this.resource = resource;
+            this.week = objects;
+            layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, @androidx.annotation.Nullable View convertView, @androidx.annotation.NonNull ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                holder = new ViewHolder();
+                convertView = layoutInflater.inflate(resource, null);
+                holder.ivlogo = (LetterImageView)convertView.findViewById(R.id.ivLetter);
+                holder.tvWeek = (TextView)convertView.findViewById(R.id.tvMain);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+            return convertView;
+        }
+        class ViewHolder{
+            private LetterImageView ivlogo;
+            private TextView tvWeek;
+    }
     }
 }
