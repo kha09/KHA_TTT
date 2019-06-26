7dedc7f2932a2bf85ba6e27cd763cff6c36297fc
Tomato <gamerx6900@gmail.com>
2019-06-26 21:23:26 +0300

    FacultyActivity, Continue 18:00

--- app/src/main/java/com/example/timetable2/FacultyActivity.java
+++ app/src/main/java/com/example/timetable2/FacultyActivity.java
@@ -1,14 +1,25 @@
 package com.example.timetable2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.timetable2.utils.LetterImageView;
 
 public class FacultyActivity extends AppCompatActivity {
     private ListView listview;
@@ -33,13 +44,12 @@ public class FacultyActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     private void setupListView(){
-        String[] title = getResources().getStringArray(R.array.Week);
-        String[] description = getResources().getStringArray(R.array.Day);
+        String[] faculty_names = getResources().getStringArray(R.array.faculty_name);
 
         // String[] week = getResources().getStringArray(R.array.Week);
-        SimpleAdapter2 simpleAdapter = new SimpleAdapter2(this, title, description);
-        listView.setAdapter(simpleAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        FacultyAdapter simpleAdapter = new FacultyAdapter(this, R.layout.faculty_single_item, faculty_names);
+        listview.setAdapter(simpleAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
                 switch(i){
@@ -66,6 +76,45 @@ public class FacultyActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public class FacultyAdapter extends ArrayAdapter {
+
+        private int resource;
+        private LayoutInflater layoutInflater;
+        private String[] faculty = new String[]{};
+
+        public FacultyAdapter(Context context, int resource, String[] objects) {
+            super(context, resource, objects);
+            this.resource = resource;
+            this.faculty = objects;
+            layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                holder = new ViewHolder();
+                convertView = layoutInflater.inflate(resource, null);
+                holder.ivLogo = (LetterImageView)convertView.findViewById(R.id.ivLetterFaculty);
+                holder.tvFaculty = (TextView)convertView.findViewById(R.id.tvFacultyName);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            holder.ivLogo.setOval(true);
+            holder.ivLogo.setLetter(faculty[position].charAt(0));
+            holder.tvFaculty.setText(faculty[position]);
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            private LetterImageView ivLogo;
+            private TextView tvFaculty;
+        }
     }
 
 }
